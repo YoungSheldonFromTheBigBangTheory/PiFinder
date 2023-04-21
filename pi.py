c80078d4794e79a0180b48a8a053c14878d165f6
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-21 13:03:36 +0000

    m

--- pi.py
+++ pi.py
@@ -1,1 +1,19 @@
+import math
 
+while True:
+    d = input("Enter Diameter: ")
+    try:
+        d = float(d)
+    except ValueError:
+        print("\nInvalid\n")
+        continue
+
+    r = d/2
+
+    A = math.pi * (r**2)
+    C = 2 * math.pi * r
+
+    print("The Area is " + str(A))
+    print("The Circumference is " + str(C))
+
+    break
